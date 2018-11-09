e3120060d4f51cbb040cac9f070bb4bd7126400d
harshaveni <harshaveni.l@softvision.com>
2018-11-09 12:20:05 +0530

    BTAT Assessment Commit

---
+++ src/main/java/SampleMaven/Java_File.java
@@ -0,0 +1,10 @@
+package SampleMaven;
+
+public class Java_File {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/SampleMaven/Test_Java_File.java
@@ -0,0 +1,14 @@
+package SampleMaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_Java_File {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
